f73078282e497dc6b4e8838fecfad7d8b5a9a510
leyi <leyi@myun.info>
2021-12-25 21:13:45 +0800

    ✨特性: first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,20 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ msg1 }}</h1>
 
   <p>
-    Recommended IDE setup:
+    Recommended IDE setup:{{ count }}
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
     +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
   </p>
 
-  <p>See <code>README.md</code> for more information.</p>
+  <p>
+    See
+    <code>README.md</code> for more information.
+  </p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
@@ -33,20 +26,40 @@ const count = ref(0)
   </p>
 </template>
 
+<script lang="ts">
+  import { defineComponent, ref, toRefs } from 'vue'
+
+  export default defineComponent({
+    props: {
+      msg: {
+        type: String,
+      },
+    },
+    setup(props) {
+      const count = ref(0)
+      const { msg } = toRefs(props)
+      return {
+        count,
+        msg1: msg,
+      }
+    },
+  })
+</script>
+
 <style scoped>
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
+  label {
+    margin: 0 0.5em;
+    font-weight: bold;
+  }
 
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
+  code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+  }
 </style>
