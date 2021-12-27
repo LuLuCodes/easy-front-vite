4bec3df270cc74bc9fadbf5fdc0ef020645cdbe7
leyi <leyi@myun.info>
2021-12-27 13:28:27 +0800

    ✨特性: 编译配置

---
+++ types/global.d.ts
@@ -0,0 +1,19 @@
+declare type Recordable<T = any> = Record<string, T>
+
+interface ImportMetaEnv extends ViteEnv {
+  __: unknown
+}
+
+declare interface ViteEnv {
+  VITE_PORT: number
+  VITE_PUBLIC_PATH: string
+  VITE_PROXY: [string, string][]
+  VITE_GLOB_APP_TITLE: string
+  VITE_GLOB_APP_SHORT_NAME: string
+  VITE_USE_CDN: boolean
+  VITE_DROP_CONSOLE: boolean
+  VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'none'
+  VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean
+  VITE_LEGACY: boolean
+  VITE_USE_IMAGEMIN: boolean
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,84 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import type { UserConfig, ConfigEnv } from 'vite'
+import pkg from './package.json'
+import dayjs from 'dayjs'
+import { loadEnv } from 'vite'
 import { resolve } from 'path'
+import { createProxy } from './build/vite/proxy'
+import { wrapperEnv } from './build/utils'
+import { createVitePlugins } from './build/vite/plugin'
 
-// npm i --save-dev @types/node
 function pathResolve(dir: string) {
   return resolve(process.cwd(), '.', dir)
 }
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    // 路径别名
-    alias: [
-      {
-        find: /\/@\//,
-        replacement: pathResolve('src') + '/',
+const { name, version } = pkg
+const __APP_INFO__ = {
+  pkg: { name, version },
+  lastBuildTime: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+}
+
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd()
+
+  const env = loadEnv(mode, root)
+
+  // The boolean type read by loadEnv is a string. This function can be converted to boolean type
+  const viteEnv = wrapperEnv(env)
+
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv
+
+  const isBuild = command === 'build'
+
+  return {
+    base: VITE_PUBLIC_PATH,
+    root,
+    resolve: {
+      alias: [
+        // /@/xxxx => src/xxxx
+        {
+          find: /\/@\//,
+          replacement: pathResolve('src') + '/',
+        },
+        // /#/xxxx => types/xxxx
+        {
+          find: /\/#\//,
+          replacement: pathResolve('types') + '/',
+        },
+      ],
+    },
+    server: {
+      // Listening on all local IPs
+      host: true,
+      port: VITE_PORT,
+      // Load proxy configuration from .env
+      proxy: createProxy(VITE_PROXY),
+    },
+    build: {
+      target: 'es2015',
+      cssTarget: 'chrome86',
+      outDir: 'dist',
+      terserOptions: {
+        compress: {
+          keep_infinity: true,
+          // Used to delete console in production environment
+          drop_console: VITE_DROP_CONSOLE,
+        },
       },
-      {
-        find: /\/#\//,
-        replacement: pathResolve('types') + '/',
+      // Turning off brotliSize display can slightly reduce packaging time
+      brotliSize: false,
+      chunkSizeWarningLimit: 2000,
+    },
+    define: {
+      __APP_INFO__: JSON.stringify(__APP_INFO__),
+    },
+
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
       },
-    ],
-  },
-  // 支持使用tsx语法
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-    jsxInject: 'import { h } from "vue";',
-  },
-})
+    },
+    plugins: createVitePlugins(viteEnv, isBuild),
+  }
+}
