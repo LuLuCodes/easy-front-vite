7dc138b552477f445496be685cdc03169addb195
leyi <leyi@myun.info>
2022-02-15 18:02:32 +0800

    ✨特性(icon support size): icon support size

--- src/components/global/IconSvg.vue
+++ src/components/global/IconSvg.vue
@@ -3,19 +3,30 @@ interface Props {
   prefix?: string
   name?: string
   color?: string
+  size?: number
 }
 
 const props = withDefaults(defineProps<Props>(), {
   prefix: 'icon',
   name: '',
   color: '#000',
+  size: 24,
 })
 
 const symbolId = computed(() => `#${props.prefix}-${props.name}`)
+const getStyle = computed(() => {
+  const { size } = props
+  let s = `${size}`
+  s = `${s.replace('px', '')}px`
+  return {
+    width: s,
+    height: s,
+  }
+})
 </script>
 
 <template>
-  <svg aria-hidden="true">
+  <svg aria-hidden="true" :style="getStyle">
     <use :xlink:href="symbolId" :fill="color" />
   </svg>
 </template>
