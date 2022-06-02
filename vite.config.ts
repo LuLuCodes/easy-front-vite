06adb6c1f859463acec7fc3839244dafe76fa7ec
leyi <leyi@myun.info>
2022-06-02 15:35:32 +0800

    ✨特性: css modules

--- vite.config.ts
+++ vite.config.ts
@@ -73,6 +73,9 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
     },
 
     css: {
+      modules: {
+        generateScopedName: '[name]__[local]___[hash:base64:5]',
+      },
       preprocessorOptions: {
         less: {
           javascriptEnabled: true,
